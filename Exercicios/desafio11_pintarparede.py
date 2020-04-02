ad5fa6fcb01a15ff0615a62ce954e0cfe33b531f
André Luiz Barros <lbarros.andre@gmail.com>
2020-04-02 11:36:25 -0300

    Subindo arquivos do dia 02/04

---
+++ Exercicios/desafio11_pintarparede.py
@@ -0,0 +1,5 @@
+largura = float(input('Qual a largura da parede: '))
+altura = float(input('Qual a altura da parede: '))
+area = largura*altura
+tinta = area/2
+print('A área é de {} m2, e será necessário {} litros de tinta para pintar a parede'.format(area, tinta))
---
+++ Exercicios/desafio12_desconto.py
@@ -0,0 +1,3 @@
+preco = float(input('Qual o preço do produto? '))
+desconto = preco*0.95
+print('Você tem 5% de desconto e o novo preço é: R$ {:.2f}'.format(desconto))
