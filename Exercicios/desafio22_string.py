ef6568b916bd0349e4bc855db5142bee6f7e0a55
André Luiz Barros <lbarros.andre@gmail.com>
2020-04-08 12:03:01 -0300

    Atualização Exercicio 22

---
+++ Exercicios/desafio22_string.py
@@ -0,0 +1,14 @@
+# exercicios strong's
+name = input('Digite seu nome completo: ')
+print(name.upper())
+print(name.lower())
+print(len(name.strip()))
+print(len(name.replace(" ", "")))
+print('Quantas letras tem meu primeiro nome:', len(name.split()[0]))
+
+
+
+
+
+
+
