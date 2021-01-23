133694a453a504aa7f2f4efc70126cc5e09abed0
andre <lbarros.andre@gmail.com>
2021-01-22 22:05:35 -0300

    Create desafio74_sorteio.py

---
+++ Exercicios/desafio74_sorteio.py
@@ -0,0 +1,7 @@
+from random import randint
+dados = (randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9), randint(1, 9))
+print(f'O sorteio dos 5 números são: {dados}')
+print(f'O valor maximo sorteado foi {max(dados)} \nO valor minimo sorteado foi {min(dados)}')
+
+
+
