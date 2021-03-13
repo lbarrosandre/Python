454bea04f17404e114af1ddbe59f65f04997d90b
andre <lbarros.andre@gmail.com>
2021-03-13 09:35:33 -0300

    Create desafio90_dicionario.py

---
+++ Exercicios/desafio90_dicionario.py
@@ -0,0 +1,10 @@
+aluno = dict()
+aluno['nome'] = input('Nome: ')
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+
+print(f'Nome é igual a {aluno["nome"]}')
+print(f'Média é igual a {aluno["media"]}')
+if aluno['media'] >= 7:
+    print('Situação é igual a Aprovado')
+else:
+    print('Situação é igual a Reprovado')
